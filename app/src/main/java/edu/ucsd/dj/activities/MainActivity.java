ae4b59a0e184e0569bdcf55f1b0a491f8c245872
Josh <j6hunter@ucsd.edu>
2017-06-09 01:23:50 -0700

    DJ-117 Photos saving (somewhat correctly). Trying to get the pictures save to the correct directory.

--- app/src/main/java/edu/ucsd/dj/activities/MainActivity.java
+++ app/src/main/java/edu/ucsd/dj/activities/MainActivity.java
@@ -3,18 +3,29 @@ package edu.ucsd.dj.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
-import java.util.List;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import edu.ucsd.dj.R;
 
@@ -35,6 +46,8 @@ import edu.ucsd.dj.others.PhotoCollection;
 import edu.ucsd.dj.managers.DJWallpaper;
 import edu.ucsd.dj.managers.Settings;
 
+import static edu.ucsd.dj.others.FileUtilities.copy;
+
 /**
  * Main activity. The 'Settings' page.
  */
@@ -43,7 +56,10 @@ public class MainActivity extends AppCompatActivity{
     private static final int SET_WALLPAPER_PERMISSION = 69;
     private static final int ACCESS_FINE_PERMISSION = 420;
     private static final int WRITE_STORAGE_PERMISSION = 666;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    String mCurrentPhotoPath;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
     private Switch
             proximitySwitch,
@@ -105,8 +121,8 @@ public class MainActivity extends AppCompatActivity{
         PhotoLoader loader = new PhotoLoader(Settings.getInstance().MAIN_LOCATION);
         ps.uploadPhotos( primaryUser, loader.getPhotos());
 
-        FirebaseDB db = FirebaseDB.getInstance();
-        List<Photo> temp = db.downloadAllFriendsPhotos(new DJFriends());
+//        FirebaseDB db = FirebaseDB.getInstance();
+//        List<Photo> temp = db.downloadAllFriendsPhotos(new DJFriends());
 
         proximitySwitch = (Switch) findViewById(R.id.proximitySwitch);
         timeOfDaySwitch = (Switch) findViewById(R.id.timeOfDaySwitch);
@@ -201,17 +217,19 @@ public class MainActivity extends AppCompatActivity{
                 Settings.getInstance().setRefreshRateMinutes(20 + seekBar.getProgress());
             }
         });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (picIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(picIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
         Log.i(this.getClass().toString() + ":onCreate()", "MainActivity listeners configured.");
 
-    }
-
-    public void openPicker(View view){
-        Intent intent = new Intent(MainActivity.this, PhotoPicker.class);
-        startActivity(intent);
-    }
-
-    public void openCamera(View view) {
-        Camera camera = new Camera();
     }
 
     private void askPermission(String permission, int requestCode){
@@ -285,5 +303,64 @@ public class MainActivity extends AppCompatActivity{
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = new ImageView(this);
+            mImageView.setImageBitmap(imageBitmap);
+            dispatchTakePictureIntent();
+            galleryAddPic();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "DJPhoto_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "edu.ucsd.dj.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        copy(mCurrentPhotoPath, "DCIM/");
     }
 }
